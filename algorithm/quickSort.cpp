379c950158ed03bbf4afd6c5feb828820b9bc581
yutao_guo <314876652@qq.com>
2016-05-09 14:32:53 +0800

    快排

--- algorithm/quickSort.cpp
+++ algorithm/quickSort.cpp
@@ -5,15 +5,32 @@ using namespace std;
 
 vector<int> quickSort(vector<int> arr, int l ,int r)
 {
-	
+	if(l < r)
+	{
+		int i = l;
+		int j = r;
+		int x = arr[l];
+		while(i < j)
+		{
+			while(i < j && arr[j] >= x)j--;
+			if(i < j)arr[i++] = arr[j];
+			while(i < j && arr[i] <= x)i++;
+			if(i < j)arr[j--] = arr[i]; 
+		}
+		arr[i] = x;
+		arr = quickSort(arr,l,i-1);
+		arr = quickSort(arr,i+1,r);
+	}
+	return arr;
 }
 
 int main()
 {
-	int num[] = {8,3,1,5,9,4,2};
-	vector<int> arr1(num,num+7);
+	int num[] = {8,3,1,5,9,4,2,45,7,22,23,6,-1};
+	int size = sizeof(num)/sizeof(num[0]);
+	vector<int> arr1(num,num+size);
 	vector<int> arr2;
-	arr2 = quickSort(arr1);
+	arr2 = quickSort(arr1,0,arr1.size()-1);
 	for_each(arr2.begin(),arr2.end(),[](int a){cout<< a <<" ";});
 	cout<<endl;
 	return 0;
